09dae13c7efa5dba69778605b6463de336ba8f30
Elvis1p1 <elvisokeke47@gmail.com>
2024-06-05 10:31:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) 
+{ 
+    event.preventDefault(); 
+    // Perform form validation and submission 
+    // Display success or error message 
+    alert('Fake Form submitted successfully!'); 
+});
